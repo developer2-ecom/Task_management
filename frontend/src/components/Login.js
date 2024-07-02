dfa3009d34fbc956907f521bd10672a7b0e70fad
Anjana Mourya <anjana@ecompapi.com>
2024-07-02 18:29:44 +0530

    home, login, register ui

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+
+function Login() {
+  const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  axios.defaults.withCredentials = true;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login({ email, password });
+      setTimeout(() => {
+        console.log("cookiiie", document.cookie);
+      });
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+
+    console.log("Login details", email);
+  };
+
+  return (
+    <>
+      <div className="LoginWrapper" id="login" >
+        <div>
+          <h2 style={{color:"white"}}>Login</h2>
+          <form onSubmit={handleSubmit} className="">
+            <label className="login-label">
+              Enter your email:
+              <input
+                type="email"
+                name="email"
+                className="login-input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <br />
+            <label className="login-label">
+              Enter your password:
+              <input
+                type="password"
+                name="password"
+                className="login-input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="submit" className="">
+              Login
+            </button>
+            <Link to="/forgotPassword">Forgot password?</Link>
+            <p>
+              Don't have an Account?<Link to="/register">Register here</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+
+        </>
+  );
+}
+
+export default Login;
